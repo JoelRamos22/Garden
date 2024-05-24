c76b3742c040c798605d2b6e18cefdd35541147e
JoelRamos22 <128657225+JoelRamos22@users.noreply.github.com.>
2024-05-24 14:19:14 -0500

    fix: :bug: Arreglo con las rutas de los estilos
    Se arregla la ruta de los estilos CSS
    
    fix: :bug: Arreglo con un archivo de Javascript mal en rutado
    
    se arregla un archivo de javascript mal enrutado

--- js/module/product.js
+++ js/module/product.js
@@ -1,6 +1,11 @@
 //15.Devuelve un listado con todos los productos que pertenecen a la gama Ornamentales y que tienen más de 100 unidades en stock. El listado deberá estar ordenado por su precio de venta, mostrando en primer lugar los de mayor precio.
 
 
+export const getAllProductsOrnamentales100  = async() =>{
+    let res = await fetch ("http://172.16.101.146:5516/products?gama=Ornamentales&stock_gt=100&_sort=-price_sale")
+    let data = await res.json();
+    return data
+}
 
 export async function getAllProductsByCode(code) {
     let res = await fetch(`http://172.16.101.146:5686/products?code=$code`);
